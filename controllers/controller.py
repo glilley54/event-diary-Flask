de82f40cdc4a0accb071450e8d1bb09ed4589e2a
sailorj94 <jackniall9@gmail.com>
2021-04-15 14:14:21 +0100

    Web app listing all events

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,9 @@
 from flask import render_template, request
 from app import app
+from models.event_list import events
+from models.event import Event
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title = "Diary", events = events)
