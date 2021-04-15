6d1a9266197f260f4dd11dca2365fb07abb827b5
sailorj94 <jackniall9@gmail.com>
2021-04-15 15:28:34 +0100

    Extensions complete. Working on CSS

--- controllers/controller.py
+++ controllers/controller.py
@@ -15,7 +15,10 @@ def add_event():
     new_guests = request.form['guests']
     new_room = request.form['room']
     new_description = request.form['description']
-    new_event = Event(new_name, new_date, new_guests, new_room, new_description)
+    new_recurring = False
+    if 'recurring' in request.form:
+        new_recurring = True
+    new_event = Event(new_name, new_date, new_guests, new_room, new_description, new_recurring)
     add_new_event(new_event)
 
     return render_template('index.html', title="Diary", events=events)
