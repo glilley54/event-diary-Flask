d69a8b8feffb3acd348212354a1ffb41f589d190
sailorj94 <jackniall9@gmail.com>
2021-04-15 13:59:53 +0100

    File framework complete

---
+++ models/event.py
@@ -0,0 +1,8 @@
+class Event():
+
+    def __init__(self, event_name, date, guests, room, description):
+        self.event_name = event_name
+        self.date = date
+        self.guests = guests
+        self.room = room
+        self.description = description
---
+++ models/event_list.py
@@ -0,0 +1,8 @@
+from models.event import *
+
+event1 = Event('Football', 'Apr 15th', 6, 'Pub', 'Watch football with friends.')
+event2 = Event('Party', 'Apr 16th', 20, 'Living Room', '30th Birthday Party')
+events = [event1, event2]
+
+def add_event(event):
+    events.append(event)
